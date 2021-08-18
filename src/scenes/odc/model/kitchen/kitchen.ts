0c2724e5d6e0ab8db83852aaf46a1a2365e797d3
153516741@qq.com <153516741@qq.com>
2021-08-18 19:31:40 +0800

    修改ts

--- src/scenes/odc/model/kitchen/kitchen.ts
+++ src/scenes/odc/model/kitchen/kitchen.ts
@@ -1,21 +1,23 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import {Object3D} from "three";
 
 // TODO
 export class Kitchen extends THREE.Group {
-    public constructor() {
+    public constructor({kitchenObj}: {kitchenObj: Object3D}) {
         super();
-        this.initKitchen();
+        this.add(kitchenObj)
     }
 
-    private initKitchen() {
-        const objLoader = new OBJLoader();
-        // TODO 修改为全局
-        objLoader.load('/3d-model/kitchen/kitchen.obj', (obj) => {
-            const scale = 0.8;
-            obj.scale.set(scale, scale, scale);
-            (obj.children[0] as THREE.Mesh).material.color.set(0xdeb887);
-            this.add(obj);
-        });
-    }
+  static loadKitchenResource(){
+    return new Promise((resolve) => {
+      const objLoader = new OBJLoader();
+      objLoader.load('/3d-model/kitchen/kitchen.obj', (obj) => {
+        const scale = 0.8;
+        obj.scale.set(scale, scale, scale);
+        (obj.children[0] as THREE.Mesh).material.color.set(0xdeb887);
+        resolve({ kitchenObj: obj });
+      });
+    })
+  }
 }
